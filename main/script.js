26bbe7dcbc2aa2f2c93f9310bb80f82bd9be747d
samcmcr <samchavoya@gmail.com>
2023-03-25 10:16:17 -0700

    dropdown menu etc

--- main/script.js
+++ main/script.js
@@ -1,5 +1,5 @@
 <script>
-// Add click event listeners to each section link
+  // Add click event listeners to each section link
   sectionLinks.forEach(link => {
     link.addEventListener('click', () => {
       // Get the ID of the linked section
@@ -11,5 +11,22 @@
       // Toggle the 'show' class on the section's content
       section.querySelector('.toggle-content').classList.toggle('show');
     });
-});
+  });
+
+  function updateTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    var time = hours + ':' + minutes + ':' + seconds;
+
+    document.getElementById('clock').innerHTML = time;
+  }
+
+  setInterval(updateTime, 1000);
 </script>
